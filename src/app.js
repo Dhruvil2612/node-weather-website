5e95df0b0e7047e1fc16456ce392af936e67494a
vigil <vigil@cloudzon.com>
2021-05-20 13:59:20 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Defines path for express configuration
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -102,6 +103,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is started successful on port 3000!')
+app.listen(port, () => {
+    console.log('Server is started successful on port ' +port+ '!')
 })
